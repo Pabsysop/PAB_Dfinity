184d7cf3d93cfe785c1215b3fd3343befe1cebfe
OASIS <oasis7.eos@gmail.com>
2021-11-17 02:04:32 +0800

    fix

--- src/boards/board_canister/src/inter_call/mod.rs
+++ src/boards/board_canister/src/inter_call/mod.rs
@@ -4,19 +4,6 @@ use ic_cdk::api;
 use candid::Principal;
 use nft::*;
 
-pub async fn listen_to(to: Principal, args: String) {
-    match api::call::call(to, "Listen", (args,)).await
-    {
-        Ok(x) => x,
-        Err((code, msg)) => {
-            println!(
-                "An error happened during the call: {}: {}",
-                code as u8, msg
-            );
-        }
-    };
-}
-
 pub async fn mint_visa_nft_call(nft_can: Principal, owner: Principal) -> Result<String, String>{
     let egg = NftEgg {
         payload: NFTPayload::Payload(vec![0x00]),
